47b61c198775c0e1befc9cd37f488aeaf6e3ecd5
Nplatof <148470922+Nplatof@users.noreply.github.com>
2024-10-16 09:19:10 -0400

    Add files via upload

---
+++ db.py
@@ -0,0 +1,91 @@
+import mysql.connector
+from flask import jsonify
+
+class Database:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+
+    def connect(self):
+        mydb = mysql.connector.connect(
+            host = self.host,
+            user = self.user,
+            password = self.password,
+            database = self.database
+            )
+        return mydb
+    
+    def readAll(self):
+        conn = self.connect()
+        cursor = conn.cursor()
+        req = "SELECT * FROM etudiant"
+        cursor.execute(req)
+        #print(req)
+        data = cursor.fetchall()
+        conn.close()
+        return data
+    
+    def readOne(self, id):
+        conn = self.connect()
+        cursor = conn.cursor()
+        req = f"SELECT * FROM etudiant WHERE idetudiant ='{id}'"
+        cursor.execute(req)
+        #print(req)
+        data = cursor.fetchone()
+        conn.close()
+        return data
+    
+    def create(self, nom, prenom, email, telephone):
+        conn = self.connect()
+        cursor = conn.cursor()
+        req = f"INSERT INTO etudiant (nom, prenom, email, telephone) VALUES ('{nom}','{prenom}','{email}','{telephone}')"
+        cursor.execute(req)
+        #print(req)
+        conn.commit()
+        conn.close()
+
+    def delete(self, id):
+        conn = self.connect()
+        cursor = conn.cursor()
+        try:
+            req = f"DELETE FROM `etudiant` WHERE `idetudiant`='{id}'"
+            cursor.execute(req)
+            conn.commit()
+            return cursor.rowcount  
+        except mysql.connector.Error as err:
+            conn.rollback()  
+            return 0  
+        finally:
+            conn.close()  
+
+    def update(self, nom, prenom, email, telephone, id):
+        conn = self.connect()
+        cursor = conn.cursor()
+        try:
+            req = f"UPDATE `etudiant` SET `nom`= '{nom}',`prenom`= '{prenom}',`email`= '{email}',`telephone`= '{telephone}' WHERE idetudiant = '{id}'"
+            cursor.execute(req)
+            conn.commit()
+            return cursor.rowcount  
+        except mysql.connector.Error as err:
+            conn.rollback()  
+            return 0  
+        finally:
+            conn.close()  
+    
+    def authorized(self, request):
+        auth = request.authorization
+        username = auth.username
+        password = auth.password
+        conn = self.connect()
+        cursor = conn.cursor()
+        req = f"SELECT password FROM user WHERE login = '{username}'"
+        #print(req)
+        cursor.execute(req)
+        data = cursor.fetchone()
+        conn.close()
+        if data and (data[0] == password):
+            return True
+        else:
+            return False
